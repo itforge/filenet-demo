a8a859fb6a783d004cb0a2a62298eda81a963971
Leander Janssen <leanderjanssen@users.noreply.github.com>
2022-10-04 15:03:40 +0200

    Update main.tf

--- modules/server/main.tf
+++ modules/server/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_linux_virtual_machine" "server" {
 
   admin_ssh_key {
     username   = var.admin_user
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("id_rsa.pub")
   }
 
   os_disk {
